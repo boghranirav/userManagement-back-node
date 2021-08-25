4c069d19bf085adb4095b96f2bdc40046c4da84d
john doe <john doe>
2021-08-25 11:53:01 +0530

    Use of Proxy with puppeteer

--- app/test/header.test.js
+++ app/test/header.test.js
@@ -1,22 +1,17 @@
-const puppeteer = require("puppeteer");
-
-let browser, page;
+const Page = require("./helpers/page");
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto("http://localhost:3000");
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test("Correct text -> page Login -> Text login", async () => {
-  const text = await page.$eval("h6.m-0", (el) => el.innerHTML);
-
+  const text = await page.getContentsOf("h6.m-0");
   expect(text).toEqual("Login");
 });
 
@@ -26,21 +21,7 @@ test("Correct text -> page Login -> Text login", async () => {
 // }, 12000);
 
 test("When sign In, shows logout button", async () => {
-  await page.evaluate(() => {
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbF9pZCI6ImJvZ2hyYW5pcmF2MDAxQGdtYWlsLmNvbSIsImRlc2lnbmF0aW9uX2lkIjoxLCJsb2dfaWQiOjI1MSwiaWF0IjoxNjI5ODAzMDAwLCJleHAiOjE2Mjk4MzE4MDB9.ooK3Nw89X6Dkw-m29VEq7j7XxOcXW8-q6W88REH8CEo",
-        expiration: "2021-08-26T11:23:46.707Z",
-      })
-    );
-  });
-
-  await page.goto("http://localhost:3000/dashboard");
-  await page.waitFor('a[href="/logout"]');
-
-  const text = await page.$eval('a[href="/logout"]', (el) => el.innerHTML);
-
-  expect(text).toEqual("<span>Logout</span>");
+  await page.login();
+  const text = await page.getContentsOf('a[href="/logout"] span');
+  expect(text).toEqual("Logout");
 });
---
+++ app/test/helpers/page.js
@@ -0,0 +1,43 @@
+const puppeteer = require("puppeteer");
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function (target, property) {
+        return customPage[property] || browser[property] || page[property];
+      },
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
+
+  async login() {
+    await this.page.evaluate(() => {
+      localStorage.setItem(
+        "userData",
+        JSON.stringify({
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbF9pZCI6ImJvZ2hyYW5pcmF2MDAxQGdtYWlsLmNvbSIsImRlc2lnbmF0aW9uX2lkIjoxLCJsb2dfaWQiOjI1MiwiaWF0IjoxNjI5ODcxMTcxLCJleHAiOjE2Mjk4OTk5NzF9.03ZZGRzlIxFMwM7ElGRUWGWM_4WgEcC2uA6-z-jEPkQ",
+          expiration: "2021-08-26T11:23:46.707Z",
+        })
+      );
+    });
+    await this.page.goto("http://localhost:3000/");
+    await this.page.waitForSelector('a[href="/logout"]');
+  }
+
+  async getContentsOf(selector) {
+    return this.page.$eval(selector, (el) => el.innerHTML);
+  }
+}
+
+module.exports = CustomPage;
